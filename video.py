c81d7a2a0e63d96203a1b5a0cb6340a49ae65499
Markus Konrad <markus.konrad@wzb.eu>
2018-01-24 17:33:18 +0100

    a circle driven by onsets

--- video.py
+++ video.py
@@ -10,63 +10,47 @@ CLIP_FPS = 24
 CLIP_W = 640
 CLIP_H = 480
 
-#source, samplerate = open_audio_source('audio/kiriloff-fortschritt-unmastered.wav')
-#onsets, onset_max_ampl, _ = get_onsets(source, samplerate, 0.3, max_read_sec=CLIP_SEC)
-#assert len(onsets) == len(onset_max_ampl)
+r_min = 1
+r_max = CLIP_H/2
+impulse = 30
+decay = 1        # linear decay of r per frame
+
+source, samplerate = open_audio_source('audio/kiriloff-fortschritt-unmastered.wav')
+onsets, onset_max_ampl, _ = get_onsets(source, samplerate, 0.3, max_read_sec=CLIP_SEC)
+assert len(onsets) == len(onset_max_ampl)
+
+# convert onset audio sample markers to frame numbers
+onset_frames = np.round(onsets / samplerate * CLIP_FPS).astype(np.int)
+onset_frame_ampl = dict(zip(onset_frames, onset_max_ampl))
+
+r = 0
+center = (int(round(CLIP_W/2)), int(round(CLIP_H/2)))
 
 
 def make_video_frame(t):
+    global r
+
+    fnum = round(t * CLIP_FPS)   # frame number
+
     frame = np.full((CLIP_H, CLIP_W, 3), (0, 0, 0), np.uint8)
-    return frame
 
-def make_audio_frame(t):
-    return 2 * [np.sin(440 * 2 * np.pi * t)]
-
-
-#audioclip = AudioFileClip('audio/kiriloff-fortschritt-unmastered.mp3')
-audioclip = AudioFileClip('audio/kiriloff-fortschritt-unmastered.wav')
-#audioclip = AudioClip(make_audio_frame, duration=CLIP_SEC)
-#audioclip.preview()
-
-clip = VideoClip(make_video_frame, duration=CLIP_SEC)
-audioclip = audioclip.set_duration(clip.duration)
-clip = clip.set_audio(audioclip)
-clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS)
-#clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS, audio_codec='pcm_s16le', audio_fps=samplerate)
-# clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS, audio='audio/kiriloff-fortschritt-unmastered.wav', audio_fps=samplerate)
-
-
-
-sec = 10
-fps = 24
-n_frames = fps*sec
-w = 1000
-h = 1000
-r_min = 1
-r_max = h/2
-impulse = 100
-decay = 1        # linear decay of r per frame
-#writer = cv2.VideoWriter('opencv_video.mkv', cv2.VideoWriter_fourcc(*'H264'), fps, (w, h))
-writer = cv2.VideoWriter('out/opencv_video_test.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, (w, h))
-
-center = (int(round(w/2)), int(round(h/2)))
-onset_sec  = np.array([1.0, 2.0, 5.0, 5.5])
-onset_frames = onset_sec * fps
-onset_frames = onset_frames.round().astype(np.int)
-onset_ampl = np.array([0.3, 0.2, 1.0, 0.9])
-onset_frame_ampl = dict(zip(onset_frames, onset_ampl))
-
-r = 0
-for i in range(n_frames):
-    print('frame %d' % (i+1))
-
-    frame = np.full((w, h, 3), (0, 0, 0), np.uint8)
-
-    radd = onset_frame_ampl.get(i, 0) * impulse
+    onset_ampl = onset_frame_ampl.get(fnum, 0)
+    radd = onset_ampl * impulse
     r = r + radd - decay
     r = max(min(r, r_max), r_min)
 
     cv2.circle(frame, center, int(round(r)), (255, 0, 0), -1)
 
-    writer.write(frame)
+    return frame
 
+
+clip = VideoClip(make_video_frame, duration=CLIP_SEC)
+clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS)
+
+
+audioclip = AudioFileClip('audio/kiriloff-fortschritt-unmastered.wav')
+
+clip = VideoClip(make_video_frame, duration=CLIP_SEC)
+audioclip = audioclip.set_duration(clip.duration)
+clip = clip.set_audio(audioclip)
+clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS)
