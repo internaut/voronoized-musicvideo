3ed9b6b21654e544adef5607f8400e2bd6483893
Markus Konrad <post@mkonrad.net>
2018-02-05 13:09:51 +0100

    first 70 sec. test

--- video.py
+++ video.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 import cv2
 import numpy as np
@@ -8,50 +9,62 @@ import moviepy.video.fx.all as vfx
 from voronoize import features_from_img, voronoi_from_feature_samples, lines_for_voronoi, draw_lines
 from helpers import create_frame
 
-CLIP_SEC = 10
 CLIP_FPS = 24
 CLIP_W = 640
 #CLIP_H = 480
 
 
 class VideoFrameGenerator:
-    def __init__(self, input_clip, onset_frame_ampl, fps=CLIP_FPS):
-        self.input_clip = input_clip
+    def __init__(self, scenes, onset_frame_ampl, fps=CLIP_FPS):
+        assert scenes
+        assert onset_frame_ampl
+
+        self.clips = []
+        self.cur_scene = None
+        self.cur_clip = None
+        self.clip_t = 0
+        self.scenes = scenes
+        self.w, self.h = self._setup_scenes()
+        assert len(self.clips) == len(self.scenes)
+
         self.onset_frame_ampl = onset_frame_ampl
         self.fps = fps
-
-        self.w, self.h = input_clip.size
-        self.r = 0
-
-        self.r_min = 1
-        self.r_max = self.h / 2
-        self.impulse = 30
-        self.decay = 1  # linear decay of r per frame
 
         #self.frame_layers_alpha_decay_basefactor = 0.05
         #self.frame_layers = []
 
-        self.vor_lines_features_factor = 10000
-        self.vor_lines_alpha_decay_basefactor = 0.05
+        self.vor_lines_features_factor = 20000
+        self.vor_lines_alpha_decay_basefactor = 0.025
         self.vor_lines = []    # holds tuples (voronoi lines frame, lines mask, current alpha, alpha decay factor)
 
     def make_video_frame(self, t):
         fnum = int(round(t * self.fps))   # frame number
 
-        in_frame = self.input_clip.get_frame(t)
+        onset_ampl = self.onset_frame_ampl.get(fnum, 0)
+        self._update_cur_scene(t)
 
-        gray_frame, bin_frame, features = features_from_img(in_frame, blur_radius=5)
-        gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
-        gray_frame = gray_frame.astype(np.float) / 255
+        self.clip_t += 1/self.fps
+        self.clip_t -= onset_ampl * 0.5
+        self.clip_t = max(self.clip_t, 0)
+
+        if self.clip_t > self.cur_clip.end:
+            self.clip_t = 0
+
+        #print(self.cur_clip.filename, t, self.clip_t)
+
+        in_frame = self.cur_clip.get_frame(self.clip_t)
+
+        _, bin_frame, features = features_from_img(in_frame, blur_radius=5)
+        # gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+        # gray_frame = gray_frame.astype(np.float) / 255
         # out_frame = cv2.cvtColor(bin_frame, cv2.COLOR_GRAY2BGR)   # use mask as output base
         # out_frame = out_frame.astype(np.float) / 255
-        # out_frame = create_frame(in_frame.shape[1], in_frame.shape[0], dtype=np.float)
+        out_frame = create_frame(in_frame.shape[1], in_frame.shape[0], dtype=np.float)
+        gray_frame = out_frame.copy()
 
         # out_frame = in_frame.astype(np.float) / 255        # use orig. frame as output base
         # out_frame_mask = (255-bin_frame)[:, :, np.newaxis]    # use masked orig. frame as output base
         # out_frame = out_frame_mask * (in_frame.astype(np.float) / 255)
-
-        onset_ampl = self.onset_frame_ampl.get(fnum, 0)
 
         if onset_ampl > 0:
             n_vor_features = int(round(self.vor_lines_features_factor * onset_ampl))
@@ -67,7 +80,7 @@ class VideoFrameGenerator:
             self.vor_lines.append((gray_frame, vor_lines_frame, vor_lines_mask_indices, 1.0, alpha_decay))
             #self.frame_layers.append((gray_frame, 1.0, alpha_decay))
 
-        out_frame = self._update_voronoi_lines(gray_frame)
+        out_frame = self._update_voronoi_lines(out_frame)
         out_frame = out_frame * 255
         out_frame = out_frame.astype(np.uint8)
 
@@ -103,39 +116,54 @@ class VideoFrameGenerator:
 
         return frame
 
-        # #frame = np.full((CLIP_H, CLIP_W, 3), (0, 0, 0), np.uint8)
-        #
-        # onset_ampl = self.onset_frame_ampl.get(fnum, 0)
-        # radd = onset_ampl * self.impulse
-        # self.r = self.r + radd - self.decay
-        # self.r = max(min(self.r, self.r_max), self.r_min)
-        # self.r = int(round(self.r))
-        #
-        # cv2.blur(frame, (self.r, self.r), frame)
-        #
-        # #cv2.circle(frame, center, int(round(r)), (255, 0, 0), -1)
-        #
-        # return frame
+    def _setup_scenes(self):
+        for sc_def in self.scenes:
+            clip = VideoFileClip(os.path.join('video', sc_def['video']), audio=False)
+            clip = clip.fx(vfx.resize, width=CLIP_W)
+            self.clips.append(clip)
+
+        return self.clips[0].size
+
+    def _update_cur_scene(self, t):
+        for i, sc_def in enumerate(self.scenes):
+            scene_t = sc_def['t']
+            if scene_t[0] <= t < scene_t[1] and self.cur_clip is not self.clips[i]:
+                self.cur_scene = sc_def
+                self.cur_clip = self.clips[i]
+                self.clip_t = 0
+                break
 
 
 with open('tmp/onsets.pickle', 'rb') as f:
     samplerate, onsets, onset_max_ampl, _ = pickle.load(f)
     assert len(onsets) == len(onset_max_ampl)
 
-input_clip = VideoFileClip('video/VIDEO0077.3gp', audio=False).subclip(5, 5 + CLIP_SEC)
-#input_clip = VideoFileClip('video/VID_20180110_233008.mp4', audio=False).subclip(5, 5 + CLIP_SEC)
-input_clip = input_clip.fx(vfx.resize, width=CLIP_W)
+scenes = [
+    {
+        'video': 'augen.mp4',
+        #'t': (0, 20),
+        't': (0, 58.212),
+    },
+    {
+        'video': 'flug.mp4',
+        #'t': (20, 40),
+        't': (58.212, 70),
+    }
+]
+
+#input_clip = VideoFileClip('video/stockvideotest.mp4', audio=False).subclip(0, 0 + CLIP_SEC)
+#input_clip = input_clip.fx(vfx.resize, width=CLIP_W)
 
 
 # convert onset audio sample markers to frame numbers
 onset_frames = np.round(onsets / samplerate * CLIP_FPS).astype(np.int)
 onset_frame_ampl = dict(zip(onset_frames, onset_max_ampl))
 
-frame_gen = VideoFrameGenerator(input_clip, onset_frame_ampl)
+frame_gen = VideoFrameGenerator(scenes, onset_frame_ampl)
 
 audioclip = AudioFileClip('audio/kiriloff-fortschritt-unmastered.wav')
 
-clip = VideoClip(lambda t: frame_gen.make_video_frame(t), duration=CLIP_SEC)
+clip = VideoClip(lambda t: frame_gen.make_video_frame(t), duration=70)
 audioclip = audioclip.set_duration(clip.duration)
 clip = clip.set_audio(audioclip)
 clip.write_videofile('out/moviepy_video_test.mp4', fps=CLIP_FPS)
